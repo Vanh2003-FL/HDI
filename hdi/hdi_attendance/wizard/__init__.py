e90d4dc51ed56ce59146458781a56720de9e578d
Vanh2003-FL <YTK53.A9.AnhNVV@gmail.com>
2025-11-18 13:45:33 +0700

    xoa

--- hdi/hdi_attendance/wizard/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import attendance_checkin_wizard
-from . import attendance_explanation_wizard
-from . import attendance_report_wizard
-from . import attendance_bulk_update_wizard
--- hdi_attendance_clean/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import attendance
-from . import work_location
-from . import employee
