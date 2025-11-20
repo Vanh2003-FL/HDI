9f6e69b5f453b863f8cd8bb89435f8065ad0fb8c
Vanh2003-FL <YTK53.A9.AnhNVV@gmail.com>
2025-11-20 14:33:26 +0700

    delete

--- hdi/hdi_attendance/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import hr_attendance
-from . import hr_attendance_log
-from . import hr_attendance_explanation
-from . import submission_type
-from . import hr_employee
