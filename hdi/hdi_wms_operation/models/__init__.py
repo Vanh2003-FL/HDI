328d72ee25c9939496101072bf2eb1088029b191
Tngoc-git <11240772@st.neu.edu.vn>
2025-11-27 03:16:33 +0000

    e

--- hdi/hdi_wms_operation/models/__init__.py
+++ hdi/hdi_wms_operation/models/__init__.py
@@ -2,7 +2,7 @@ from . import product_template
 from . import barcode_item
 from . import receipt_operation
 from . import transfer_operation
-from . import dispatch_operation
-from . import inventory_operation
-from . import picking_area
+# from . import dispatch_operation  # TODO: Create this file
+# from . import inventory_operation  # TODO: Create this file
+# from . import picking_area  # TODO: Create this file
 from . import stock_lot
