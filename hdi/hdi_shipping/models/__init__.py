989b33454bf02c15370c7bac74a85343adb9165c
Vanh2003-FL <YTK53.A9.AnhNVV@gmail.com>
2025-12-02 13:37:20 +0700

    delete

--- hdi/hdi_shipping/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import shipping_order
-from . import shipping_order_state_log
-from . import sender_address
-from . import shipping_service
-from . import shipment_item
--- hdi/hdi_shipping/wizards/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import shipping_order_print_wizard
